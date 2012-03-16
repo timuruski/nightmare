5586178b8a6b9527389e411b718515b41ffb0c89
Tim Uruski <timuruski@gmail.com>
2012-03-15 19:25:04 -0600

    Install Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nightmare::Application.config.secret_token = '896372f3da4fed0635985c22982e005bd6550cc7cb39664179bfeff8f35157964360475738679495dde9e9b7fc0ef64084caab5c32bf07cbd64a9f9ee343df7d'
