c276df780529f0c8002af333c9a44fa0eec5f4ac
Tim Uruski <timuruski@gmail.com>
2012-04-09 21:27:25 -0600

    Add extremely naive project velocity implementation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,13 +1,29 @@
 class Project
+  DEFAULT_VELOCITY = 10
+  REQUIRED_ITERATIONS = 3
+
   def initialize(params = {})
     @name = params.fetch(:name) { '' }
     @started_on = params.fetch(:started_on) { }
+    @archive = params.fetch(:archive) { }
   end
   
   attr_reader :name, :started_on
 
   def velocity
-    0
+    return DEFAULT_VELOCITY unless enough_iterations?
+    sum = archive.last(REQUIRED_ITERATIONS).reduce(0) { |sum,iteration| sum += iteration.points }
+    sum / REQUIRED_ITERATIONS rescue DEFAULT_VELOCITY
+  end
+
+  protected
+
+  def archive
+    @archive ||= []
+  end
+
+  def enough_iterations?
+    archive.length >= REQUIRED_ITERATIONS
   end
   
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -14,9 +14,28 @@ describe Project do
   end
 
   describe "#velocity" do
-    context "with no iterations" do
+    context "with no completed iterations" do
       subject { Project.new }
-      its(:velocity) { should == 0 }
+      its(:velocity) { should == Project::DEFAULT_VELOCITY }
     end
+
+    context "with too few completed iterations" do
+      let(:iteration) { stub(:points => 3) }
+      let(:archive) { [iteration, iteration] }
+      subject { Project.new(:archive => archive) }
+
+      its(:velocity) { should == Project::DEFAULT_VELOCITY }
+    end
+
+    context "with enough completed iterations" do
+      it "averages the last 3 iterations" do
+        iteration = stub(:points => 3)
+        archive = [iteration, iteration, iteration, iteration]
+        project = Project.new(:archive => archive)
+
+        project.velocity.should == 3
+      end
+    end
+
   end
 end
